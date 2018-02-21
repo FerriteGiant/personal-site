201869054ff8d07773a60457d0cb68b569b14f2d
yelneerg <cwgreenley@gmail.com>
2018-02-20 21:30:35 -0600

    added commands to update permissions after site update

--- updateSite.sh
+++ updateSite.sh
@@ -1,3 +1,5 @@
 rm -r ./public/
 hugo
 cp -r ./public/* /var/www/chrisgreenley.com/public_html/
+sudo chgrp -R www-data /var/www/chrisgreenley.com/public_html/
+chmod -R 750 /var/www/chrisgreenley.com/public_html/
